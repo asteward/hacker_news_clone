9aae665188c1f4f02c3d7d61d1193e31514bcab2
Austin Steward <austinsteward@aol.com>
2014-08-27 13:49:49 -0700

    Add VotesController Create function

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,16 @@
 class VotesController < ApplicationController
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      flash[:notice] = "Post upvoted."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
 
+private
+  def vote_params
+    params.require(:vote).permit(:post_id)
+  end
 end
