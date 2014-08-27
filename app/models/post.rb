e7aad32d6c6d9f1e1e5415c11470d0aa54189ba1
Austin Steward <austinsteward@aol.com>
2014-08-27 13:43:58 -0700

    Add VotesController file

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,19 +2,11 @@ class Post < ActiveRecord::Base
   has_many :votes
   validates :title, presence: true
   validates :link, presence: true
-  before_create :set_votes
   after_touch :adjust_rank
 
-  def upvote
-    self.votes += 1
-  end
-
 private
-  def set_votes
-    self.votes = 0
-  end
 
   def adjust_rank
-    self.rank = self.votes
+    self.rank = self.votes.length
   end
 end
