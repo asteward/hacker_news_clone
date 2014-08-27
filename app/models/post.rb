43f9fa05d87040485e5cc75873e65870781ddf35
Austin Steward <austinsteward@aol.com>
2014-08-27 11:29:44 -0700

    Add adjust_rank callback to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,14 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :link, presence: true
   before_create :set_votes
+  after_touch :adjust_rank
 
 private
   def set_votes
     self.votes = 0
+  end
+
+  def adjust_rank
+    self.rank = self.votes
   end
 end
