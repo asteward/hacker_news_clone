d2b78fee56eebce93f14322f35d428207a53cc86
Austin Steward <austinsteward@aol.com>
2014-08-29 16:00:56 -0700

    Add authorization check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+private
+  def authorize
+    if current_user.nil?
+      flash[:alert] = "<strong>Not authorized!</strong>".html_safe
+      redirect_to login_path
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
