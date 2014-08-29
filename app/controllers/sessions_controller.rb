6c206edc07b39416147fa7f4dc697b3fa6059ef4
Austin Steward <austinsteward@aol.com>
2014-08-29 15:32:05 -0700

    Add create sessions function to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "<strong>Hey, #{user.username}!</strong> Check out our cool new posts!".html_safe
+      redirect_to users_path
+    else
+      flash.now.alert = "Username or password is invalid."
+    end
+  end
 end
