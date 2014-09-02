143b06e275b8cd9933155004c3a15f631aa7cc0d
Austin Steward <austinsteward@aol.com>
2014-09-01 21:25:54 -0700

    Test user for only one vote per post

--- spec/features/votes_pages_spec.rb
+++ spec/features/votes_pages_spec.rb
@@ -12,4 +12,18 @@ describe "voting for a post" do
     click_button 'Vote'
     expect(page).to have_content 'upVote | 1'
   end
+
+  it "allows a signed-in user to upvote only once" do
+    visit '/sessions/new'
+    user = User.create(username: 'test', password: 'password')
+    Post.create(title: 'Google', link: 'http://www.google.com', user_id: user.id)
+    fill_in 'Username', with: 'test'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    visit '/posts'
+    click_button 'Vote'
+    click_button 'Vote'
+    expect(page).to have_content 'Whoa, Nelly!'
+  end
+
 end
