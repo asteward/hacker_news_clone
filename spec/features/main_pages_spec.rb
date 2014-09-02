83e70ad22c9f9dde694d0908eec4eb78ef439258
Austin Steward <austinsteward@aol.com>
2014-09-01 20:47:00 -0700

    Test main page

---
+++ spec/features/main_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Post comments' do
+  it "allows anyone to view post comments" do
+    visit '/sessions/new'
+    user = User.create(:username => 'test', :password => 'password')
+    post = Post.create(title: 'Google', link: 'http://www.google.com')
+    fill_in 'Username', :with => 'test'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log In'
+    post = Post.create(title: 'Google', link: 'http://www.google.com', user_id: user.id)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content "Google - #{user.username}"
+  end
+end
--- spec/features/signup_pages_spec.rb
+++ spec/features/signup_pages_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "the signin process" do
   it "signs a user in who uses the right password" do
     visit '/sessions/new'
-    user = User.create(:username => 'test', :password => 'password')
-    fill_in 'Username', :with => 'test'
-    fill_in 'Password', :with => 'password'
+    user = User.create(username: 'test', password: 'password')
+    fill_in 'Username', with: 'test'
+    fill_in 'Password', with: 'password'
     click_button 'Log In'
     page.should have_content 'Check out our cool new posts!'
   end
@@ -13,8 +13,8 @@ describe "the signin process" do
   it "gives a user an error who uses the wrong password" do
     visit '/sessions/new'
     user = User.create(:username => 'test', :password => 'password')
-    fill_in 'Username', :with => 'test'
-    fill_in 'Password', :with => 'wrong'
+    fill_in 'Username', with: 'test'
+    fill_in 'Password', with: 'wrong'
     click_button 'Log In'
     page.should have_content 'Check that username and password again.'
   end
