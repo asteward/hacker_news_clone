41ed9f880c3248b71ca1f802dd58eb184b770a12
Austin Steward <austinsteward@aol.com>
2014-09-01 20:58:52 -0700

    Test non-users attempting to comment on a post

--- spec/features/add_comments_pages_spec.rb
+++ spec/features/add_comments_pages_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 describe 'Comment on a post' do
-  it "allows anyone to view post comments" do
+  it "Allows a logged-in user to comment on a post" do
     visit '/sessions/new'
     user = User.create(:username => 'test', :password => 'password')
-    post = Post.create(title: 'Google', link: 'http://www.google.com')
+    post = Post.create(title: 'Google', link: 'http://www.google.com', user_id: user.id)
     fill_in 'Username', with: 'test'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
-    post = Post.create(title: 'Google', link: 'http://www.google.com', user_id: user.id)
     visit "/users/#{user.id}/posts/#{post.id}"
     click_link "Comment"
     fill_in 'Content', with: "I commented!"
     click_button 'Create Comment'
     expect(page).to have_content "I commented!"
+  end
+
+  it "Redirects non-users to log in page if they want to comment on a post" do
+    user = User.create(:username => 'test', :password => 'password')
+    visit '/sessions/new'
+    post = Post.create(title: 'Google', link: 'http://www.google.com', user_id: user.id)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    click_link "Comment"
+    expect(page).to have_content "Not authorized!"
   end
 end
